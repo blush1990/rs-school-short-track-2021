7affcbe2065d005bc8ecd47e8992f5cce4821049
blush1990 <evgenypopov16051995@gmail.com>
2021-04-10 01:30:42 +0300

    feat: add task solution 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,18 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  for (let i = 0; i < names.length; i++) {
+    const find = names.indexOf(names[i], i + 1);
+    if (find >= 0) {
+      names.splice(find, 1, `${names[i]}(1)`);
+      i = -1;
+    }
+    if (names[i] === `${names[0]}(1)(1)(1)`) {
+      names.splice(i, 1, `${names[0]}(2)`);
+    }
+  }
+  return names;
 }
-
+// console.log(renameFiles([]))
 module.exports = renameFiles;
