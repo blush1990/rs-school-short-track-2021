d7202235783403bddadbfc314407c78718fb9930
blush1990 <evgenypopov16051995@gmail.com>
2021-04-10 16:48:21 +0300

    feat: add task solution 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,32 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      if (arr[i + 1] === arr[i + 2]) {
+        if (arr[i + 2] === arr[i + 3]) {
+          if (arr[i + 3] !== arr[i + 4]) {
+            res.push(`4${arr[i]}`);
+            i += 3;
+          }
+        } else {
+          res.push(`3${arr[i]}`);
+          i += 2;
+        }
+      } else {
+        res.push(`2${arr[i]}`);
+        i++;
+      }
+    } else {
+      (
+        res.push(arr[i])
+      );
+    }
+  }
+  return res.join('');
 }
-
+// console.log(encodeLine('aaaatttt'))
 module.exports = encodeLine;
